ad61fc1e6351d9361c1ec5cff5e7c4cb1fa758e8
Rafaela Ribeiro <rapha11br@gmail.com>
2021-11-11 14:02:53 -0300

    Validando cadastro da categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,6 +7,11 @@ const categoriesRepositories = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
+
+    const categoryAlreadyExists = categoriesRepositories.findByName(name);
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ error: "Category Already Exists " });
+    }
     categoriesRepositories.create({ name, description });
 
     return response.status(201).send();
